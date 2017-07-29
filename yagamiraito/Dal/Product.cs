43ff0eda560ffb1ef7ab11904d29b1a6480b598d
moonheart <fzp1205561571@hotmail.com>
2017-07-30 00:01:28 +0800

    + add DataBase

---
+++ yagamiraito/Dal/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yagamiraito.Dal
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ yagamiraito/Dal/YagamiDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace yagamiraito.Dal
+{
+    public class YagamiDbContext : DbContext
+    {
+        public YagamiDbContext():base(GetDbContextOptions())
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        private static DbContextOptions GetDbContextOptions()
+        {
+            var conf = new DbContextOptions<YagamiDbContext>();
+            return conf;
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
