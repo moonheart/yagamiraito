3956ebeb9bc25479274910bbadeecf0d1f3f7c49
moonheart <fzp1205561571@hotmail.com>
2017-07-30 00:31:46 +0800

    + don't use DbSetName as TableName

--- yagamiraito/Dal/YagamiDbContext.cs
+++ yagamiraito/Dal/YagamiDbContext.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace yagamiraito.Dal
 {
     public class YagamiDbContext : DbContext
     {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var internalBuilder = modelBuilder.GetInfrastructure();
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                internalBuilder
+                    .Entity(entity.Name, ConfigurationSource.Convention)
+                    .Relational(ConfigurationSource.Convention)
+                    .ToTable(entity.ClrType.Name);
+            }
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
             optionsBuilder.UseMySql("Server=45.76.54.192;database=yagami;uid=root;pwd=maria@fzp;");
